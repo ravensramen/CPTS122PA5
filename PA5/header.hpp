da71c62a01cc4ad09ecf3330b36ae372a2af4a28
ravensramen <ravensramen@gmail.com>
2025-02-27 14:22:46 -0800

    definitions for queue componenets added

--- PA5/header.hpp
+++ PA5/header.hpp
@@ -1,5 +1,7 @@
 //main header, include libraries, macros and preprocessor directives
 
+//includes definition for basic structure of each grocery line queue
+
 #ifndef HEADER_HPP
 #define HEADER_HPP
 
@@ -11,6 +13,62 @@ using std::cin;
 using std::endl;
 
 
+class Data
+{
+public: 
+	Data(); //constructor
+	~Data(); //destructor
+	//setters + getters?
 
+private:
+
+	int customerNumber;
+	int serviceTime;
+	int totalTime;
+};
+
+Data::Data() {
+	//constructor def
+}
+Data::~Data() {
+	//destructor def
+}
+
+class QueueNode {
+public:
+	QueueNode();
+	~QueueNode();
+private:
+	Data* pData;
+	QueueNode* pNext;
+
+};
+
+QueueNode::QueueNode(){
+	//constructor
+}
+
+QueueNode::~QueueNode() {
+	//destructor
+}
+
+class Queue {
+
+public:
+	Queue();
+	~Queue();
+
+private: 
+	QueueNode* pHead;
+	QueueNode* pTail;
+
+};
+
+Queue::Queue(){
+	//constructor
+}
+Queue::~Queue() {
+	//destructor
+}
 
 #endif
